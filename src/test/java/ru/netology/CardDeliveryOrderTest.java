8b41e34f4e3f8e3cbd06159353f53e9f0eed82b9
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2021-12-18 17:01:34 +0300

    Initial commit

---
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -0,0 +1,37 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryOrderTest {
+
+    @Test
+    public void shouldSendCompletedForm() {
+        Configuration.holdBrowserOpen = true;
+        //Открыть приложение
+        open("http://localhost:7777");
+        //Заполнить поле Город
+        $("[data-test-id=\"city\"] .input__control").setValue("Москва");
+        //Заполнить поле Дата встречи спредставителем банка
+        $("[data-test-id='date'] .input__control").setValue("22.12.2021");
+        //Заполнить поле Фамилия Имя
+        $x("//*[@name=\"name\"]").setValue("Иванов Василий");
+        //Заполнить поле Мобильный телефон
+        $x("//*[@name=\"phone\"]").setValue("+79998885566");
+        //Кликнуть чекбокс
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        //Нажать кнопку Забронировать
+        $(".button__text").click();
+        //Подождать 15 сек
+        $("[data-test-id=\"notification\"]").shouldHave(text("Успешно! Встреча успешно забранирована на 22.12.2021"), Duration.ofSeconds(15));
+        //Получить всплывающее окно с сообщением "Успешно! Встреча успешно забранирована на
+
+    }
+}
